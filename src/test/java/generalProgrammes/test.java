def5598d168c5ed9a2414d606b947b969653cf26
ASHISH <apple@AshishGoyal.local>
2024-10-08 19:18:27 +0530

    Prime Number

--- src/test/java/generalProgrammes/test.java
+++ src/test/java/generalProgrammes/test.java
@@ -3,16 +3,18 @@ package generalProgrammes;
 public class test {
 
 	public static void main(String[] args) {
-    	String name = "madam";
-    	String temp = "";
-    	
-    	for(int i = name.length()-1;i>=0;i--) {
-    		temp=temp + name.charAt(i);
-    	}
-    	if(name.equals(temp)) {
-    		System.out.println("Palindrome");
-    	} else {
-    		System.out.println("not");
-    	}
-    }
+		System.out.println(isPrime(3));
+	}
+
+	private static boolean isPrime(int num) {
+		if (num <= 1) {
+			return false;
+		}
+		for (int i = 2; i < num; i++) {
+			if (num % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
